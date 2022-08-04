ea1b2ed9fbd4a6d9e4909af094e0c0e56029a1c9
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-04 20:49:11 -0300

    more c++ exercices

--- c++ projects/main.cc
+++ c++ projects/main.cc
@@ -219,3 +219,40 @@ int main()
     cout<<"Character at last position: "<<str.at(length-1)<<endl;
 
 }
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void){
+    
+    // Valores a, b y c ingresados por el usuario.
+    // x1, x1 seran los valores del resultado.
+    float a, b, c, x1, x2;
+    
+    cout<<"Ingresa el valor de a: "<<endl;
+    cin>>a;
+    
+    cout<<"Ingresa el valor de b: "<<endl;
+    cin>>b;
+    
+    cout<<"Ingresa el valor de c: "<<endl;
+    cin>>c;
+    
+    // Como nos indica la formula: 
+    // restamos el valor de b elevado a 2 (b*b), con el valor de 4 * a * c
+    float x = (b*b) - (4*a*c); 
+    
+    if (x <= 0){
+        x = x*(-1);
+        cout<<"Solución solo en números complejos"<<endl;
+        cout<<"Solución en numeros complejos: " <<(-b/(2*a))<<" + "<<(sqrt(x)/(2*a))<<"i y "<<(-b/(2*a))<<" - "<<(sqrt(x)/(2*a))<<"i"<<endl;
+    }else{
+        // Aplicamos la formula
+        x1 = (-b + sqrt(x)) / (2*a);
+        x2 = (-b - sqrt(x)) / (2*a);
+        
+        cout<<"x1 = "<<x1<<endl;
+        cout<<"x2 = "<<x2<<endl;
+    }
+}
