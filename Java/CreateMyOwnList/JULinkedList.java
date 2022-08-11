8b2d32f68b69863fa121f88e7dbcf9555c50f29a
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-11 19:19:23 -0300

    update java activity

--- Java/CreateMyOwnList/JULinkedList.java
+++ Java/CreateMyOwnList/JULinkedList.java
@@ -49,7 +49,14 @@ public class JULinkedList <T> implements List {
         if(head==null){
             return false;
         }
+        Node current = head;
+        while(current != null && !current.data.equals(o)){
 
+            current = current.next;
+        }
+        if(current.data.equals(o)){
+            return true;
+        }
         return false;
     }
 
@@ -85,8 +92,26 @@ public class JULinkedList <T> implements List {
 
     @Override
     public boolean remove(Object o) {
+        Node current = head;
+        Node temp = null;
+        if(o == head.data){
+            head = current.next;
+            return true;
+        }
+
+        while(current != null && !current.data.equals(o)){
+            temp = current;
+            current = current.next;
+        }
+
+        if(current == null){
+            return true;
+        }
+
+        temp.next = current.next;
         return false;
     }
+
 
 
 
@@ -108,12 +133,57 @@ public class JULinkedList <T> implements List {
 
     @Override
     public Object get(int index) {
-        return null;
+        if (head == null){
+            return null;
+        }
+
+
+        Node temp = head;
+
+
+        if(index < 0 || index >=size()){
+            return null;
+        }else if(index == 0){
+
+
+            return head.data;
+        }else {
+
+            Node previous = head;
+            int count = 1;
+            while(count < index - 1){
+                previous = previous.next;
+                count++;
+            }
+
+            Node current = previous.next;
+
+            return current.data;
+        }
     }
 
     @Override
     public Object set(int index, Object element) {
-        return null;
+        Node node = head;
+
+        if(index == 1){
+            node.next = head;
+            head = node;
+        } else {
+             Node previous = head;
+            int count = 1; // position - 1
+
+            while(count < index - 1){
+                previous = previous.next;
+                count++;
+            }
+
+            Node current = previous.next;
+            previous.next = node;
+            node.next = current;
+        }
+
+        return element;
     }
 
     @Override
@@ -123,11 +193,39 @@ public class JULinkedList <T> implements List {
 
     @Override
     public Object remove(int index) {
-        return null;
+        if (head == null){
+            return null;
+        }
+
+
+        Node temp = head;
+
+
+        if(index < 0 || index >=size()){
+            return null;
+        }else if(index == 0){
+
+            head = head.next;
+            temp.next = null;
+            return temp;
+        }else {
+
+            Node previous = head;
+            int count = 1;
+            while(count < index - 1){
+                previous = previous.next;
+                count++;
+            }
+
+            Node current = previous.next;
+            previous.next = current.next;
+            return current.data;
+        }
     }
 
     @Override
     public int indexOf(Object o) {
+
         return 0;
     }
 
--- Java/CreateMyOwnList/Main.java
+++ Java/CreateMyOwnList/Main.java
@@ -12,9 +12,32 @@ public class Main {
 
         lista.add(1);
         lista.add(2);
+        lista.add(3);
 
         System.out.println(lista.size());
+
+        lista.remove(1);
+        System.out.println(lista.size());
+
         System.out.println(lista.isEmpty());
+
+        System.out.println("-------String List -------------");
+
+        List<String> lista2 = new JULinkedList();
+        System.out.println(lista2.isEmpty());
+
+        lista2.add("1");
+        lista2.add("2");
+        lista2.add("3");
+
+        System.out.println(lista2.size());
+
+        lista2.remove("1");
+        lista2.get(0);
+        System.out.println(lista2.contains("3"));
+        System.out.println(lista2.size());
+
+        System.out.println(lista2.isEmpty());
 
 
     }
