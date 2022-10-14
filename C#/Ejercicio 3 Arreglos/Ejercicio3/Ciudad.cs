37bd2c56c7009cdc5c4b598c56842bfe8af3652b
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-10-14 19:01:25 -0300

    finished ejercicios 3 arreglos

--- C#/Ejercicio 3 Arreglos/Ejercicio3/Ciudad.cs
+++ C#/Ejercicio 3 Arreglos/Ejercicio3/Ciudad.cs
@@ -7,14 +7,16 @@ namespace Ejercicio3
         int Codigo_Postal;
         string Nombre;
         int Prefijo_telefonico;
+        Provincia provincia;
 
         List<Persona> Personas = new List<Persona>();
 
-        public Ciudad(int Codigo_Postal, string Nombre, int prefijo_telefonico)
+        public Ciudad(int Codigo_Postal, string Nombre, int prefijo_telefonico,  Provincia provincia)
         {
             this.Codigo_Postal = Codigo_Postal;
             this.Nombre = Nombre;
-            this.Prefijo_telefonico = prefijo_telefonico;  
+            this.Prefijo_telefonico = prefijo_telefonico;
+            this.provincia = provincia;  
         }
         public int DevolverNumeroMasPrefijo(int Telefono) => Convert.ToInt32($"{Prefijo_telefonico}{Telefono}");
         public void AdicionarPersona(Persona persona)
