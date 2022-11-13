a2923d02d2470619fbd4fb2c869202d566e82af5
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-11-13 19:47:59 -0300

    update projects

--- Python/SeleniumLogin/bot.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://web.whatsapp.com/")
--- Python/SeleniumLogin/main.py
+++ Python/SeleniumLogin/main.py
@@ -1,9 +1,34 @@
-"""
-Main
-"""
+import time
+import sys
+import subprocess
 
 
-with open("usuario.txt") as f:
-    senha= f.readline()
-    contas = f.readlines()
+def modoAviao():
+    pass
 
+
+
+def main():
+    with open(sys.argv[sys.argv.index('-n') + 1],'r') as f:
+        contas = f.read().splitlines()
+    link = sys.argv[sys.argv.index('-l') + 1]
+    password = sys.argv[sys.argv.index('-p') + 1]
+    num = 2
+    proceso = 1
+    while True:
+
+    
+        lista = contas[num-1:num]
+        print(lista)
+        if not lista:
+            break
+
+        comand=f"py controladorBots.py {proceso} {link} {password}"
+        for i in lista:
+            comand+=f' {i}'
+        subprocess.run(comand)
+        modoAviao()
+        time.sleep(50)
+        proceso+=1
+        num +=1
+main()
