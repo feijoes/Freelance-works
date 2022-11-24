36229e0e80819a7c20e111ae959fc0b44007a7fc
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-11-23 22:19:42 -0300

    s

---
+++ C#/GUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+
+        String mes;
+        String dia;
+        String ano;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(ano);
+            Console.WriteLine(mes);
+            Console.WriteLine(dia);
+            DateTime date = DateTime.Parse(dia + "/" + mes + "/" +ano);
+            TimeSpan myAge = DateTime.Now.Subtract(date);
+            MessageBox.Show("You are " + myAge.TotalDays + " days old!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            this.dia= textBox2.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.ano = textBox1.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            this.mes = textBox3.Text;
+        }
+    }
+}
