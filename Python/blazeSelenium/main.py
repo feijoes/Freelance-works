ea4043d8d5a3328f175b70b99dad9f961e7d4298
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-01-28 17:58:35 -0300

    organize files and add api

--- Python/blazeSelenium/main.py
+++ Python/blazeSelenium/main.py
@@ -1,10 +1,18 @@
-import os
-
-def main():
+import seleniumBot
+import requests
+  
+def login():
     with open(".env") as f:
         lines: list[str] = [line.strip("\n") for line in f.readlines()]
         user , password = lines[0] ,lines[1]
-        
-    os.system(f"seleniumBot.py {user} {password}")
+    seleniumBot.login(user,password)
 
-main()
+#getDouble()
+
+# api-endpoint
+URL = "http://127.0.0.1:8000"
+  
+# sending get request and saving the response as response object
+r = requests.get(url = URL)
+print(r.json())
+
--- Python/blazeSelenium/seleniumBot.py
+++ Python/blazeSelenium/seleniumBot.py
@@ -3,30 +3,41 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from time import sleep
-import sys
-user , password = sys.argv[1:]
 
 # options
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('--profile-directory=Default')
-chrome_options.add_argument("--disable-plugins-discovery")
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_experimental_option('useAutomationExtension', False)
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+def getDriver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--profile-directory=Default')
+    chrome_options.add_argument("--disable-plugins-discovery")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-driver: webdriver.Chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) 
-try:
+    driver: webdriver.Chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) 
+    return driver
+
+def login(user: str,password: str):
+    driver =  getDriver()
     driver.get("https://blaze.com/pt/?modal=auth&tab=login")
 
     driver.find_element(By.XPATH, './/*[@id="auth-modal"]/div/form/div[1]/div/input').send_keys(user) 
     sleep(2)
     driver.find_element(By.XPATH, '//*[@id="auth-modal"]/div/form/div[2]/div/input').send_keys(password) 
     sleep(2)
-    driver.find_element(By.XPATH, '//*[@id="auth-modal"]/div/form/div[4]/button').click() 
-except Exception as e:
-    print(e)
+    driver.find_element(By.XPATH, '//*[@id="auth-modal"]/div/form/div[4]/button').click()
+    driver.close() 
 
-sleep(100)
-driver.close()
+def getDouble():
+    driver = getDriver()
+    driver.get("https://blaze.com/pt/games/double")
+    sleep(10)
+    lastNumber = driver.find_element(By.XPATH,'//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div/div').text
+    print(lastNumber)
+    
+    
+
+
+
+
