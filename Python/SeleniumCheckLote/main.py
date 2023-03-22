54a1c2173067d88e054474f8814f4037c7dbb08c
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-03-22 20:03:38 -0300

    update

--- Python/SeleniumCheckLote/main.py
+++ Python/SeleniumCheckLote/main.py
@@ -1,1 +1,10 @@
 import seleniumBot
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+CLAVE = os.getenv('CLAVE')
+with open("numeros.txt") as f:
+    numeros = [x.strip() for x in f.readlines()]
+
+seleniumBot.check_numero(numeros,CLAVE)
--- Python/SeleniumCheckLote/seleniumBot.py
+++ Python/SeleniumCheckLote/seleniumBot.py
@@ -1,20 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+import pyautogui
+from pytesseract import pytesseract
+from time import sleep
+import os
+import cv2 as cv
+URL = "https://www.bbva.pe/personas/olvido-contrasena.html"
+inputNumeroImg = os.path.dirname(os.path.abspath(__file__)) + "\\img\\numeroInput.jpeg"
+inputClaveImg = os.path.dirname(os.path.abspath(__file__)) + "\\img\\ClaveInput.jpeg"
+def write_in_input(img: str, text:str,wait:int):
+    pyautogui.moveTo(list(img))
+    pyautogui.click()
+    sleep(wait)
+    pyautogui.write(text)
+    sleep(wait)
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('--profile-directory=Default')
-chrome_options.add_argument("--disable-plugins-discovery")
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_experimental_option('useAutomationExtension', False)
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-URL = ""
-
-import seleniumBot
-
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-
+def check_numero(numeros : list[int],clave: str) -> None:
+    try:
+        #for numero in numeros:
+        #    for _ in range(20): 
+        #        inputNumero = pyautogui.locateOnScreen(inputNumeroImg,confidence=.5)
+        #        if inputNumero:
+        #            break
+        #        sleep(0.5)
+#
+        #    write_in_input(inputNumeroImg,numero,0.5)
+        #    sleep(1)
+        #    write_in_input(inputClaveImg,clave,0.5)
+        
+        pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        
+       
+ 
+        os.system("convert test.png --morphology Erode Disk:2 captcha5.tif")
+        os.system("convert test.png --morphology Dilate Disk:1.5 captcha5.tif")
+        os.system(" convert test.png --threshold 41% captcha5.tif")
+        
+        pyautogui.screenshot("test.png",region=(290,600, 270, 130))
+        img = cv.imread("./test.png")
+    
+        text:str = pytesseract.image_to_string(img,config=f'-l eng --psm 12 -c tessedit_char_whitelist=0123456789abcdfghijkmnlopqrsturstuvwxyz')
+        print(text.split())
+    
+            
+            
+            
+    except Exception as E:
+        print(E)
+        sleep(100)
+    
+    
